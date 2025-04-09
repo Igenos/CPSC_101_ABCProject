3b4eafd8be54d2c1a29c477d6dedab1b0f37d8d0
Caleb <caleb.g.hull@gmail.com>
2025-04-08 18:38:02 -0700

    Completed game, made it scalable, Added logo for some reason, removed redundancies and errors with player turn display.

--- Grid_Game/main.py
+++ Grid_Game/main.py
@@ -1,12 +1,20 @@
 
-from abc import ABC, abstractmethod
-from lib.Grid_GameABC import Grid_Game, Board_State, Move
+# Import assests from the ABC. Includes Board_State, Move, and Grid_Game
+from lib.Grid_GameABC import *
 
+# Used for saving games to save_directory
 import sqlite3
-import datetime
+
+# Logging dependencies
+import datetime 
 import logging
-import tkinter as tk
+
+# Used to make the game portable
 from pathlib import Path
+
+# UI dependencies
+from tkinter import *
+from tkinter import ttk
 
 class TTT_Board(Board_State):
     def __init__(self, turn = 1, current_player = 1, moves_played = None, player_count = 2, is_winner = False):
@@ -21,6 +29,8 @@ class tic_tac_toe(Grid_Game):
         # Find out where we are.
         self.current_directory: Path = Path(__file__).parent.absolute()
         self.save_directory: Path = self.current_directory / 'current_game.db'
+        self.log_directory: Path = self.current_directory / 'tttgame.log'
+        self.image_assets_directory: Path = self.current_directory / 'game/images/'
 
         # SQLite setup
         self.connection = sqlite3.connect(self.save_directory)
@@ -35,22 +45,36 @@ class tic_tac_toe(Grid_Game):
         self.current_board: TTT_Board = TTT_Board()
 
         # Create interface objects using tkinter.
-        self.root = tk.Tk()
+        self.root = Tk()
         self.root.title("Tic-Tac-Toe")
 
-        # Create message label and reset button
-        self.message_label = tk.Label(self.root, text="Player 1's turn", font=("Helvetica", 12))
-        self.message_label.grid(row=self.y_max, column=0, columnspan=self.x_max)
+        self.main_frame = Frame(self.root, padx= 10, pady=10)
+        
+        logo_image = PhotoImage(file=self.image_assets_directory / 'ttt_logo.png')
+        # self.logo_frame = Frame(self.root, padx= 10, pady=10)
+        self.logo = ttk.Label(self.root)
+        self.logo['image'] = logo_image
+        self.logo.grid(row= 0, column=0)
 
-        self.reset_button = tk.Button(self.root, text="Reset", command=self.reset_game)
-        self.reset_button.grid(row=self.y_max + 1, column=0, columnspan=self.x_max)
+        self.main_frame.grid(row=self.y_max+3, column=0, columnspan=self.x_max, sticky=(N,W,E,S))
+        # self.main_frame.grid['padding'] = 5
+        self.root.columnconfigure(0, weight= 1)
+        self.root.rowconfigure(0, weight=1)
+        
+        # Create message label and reset button
+        self.message_label = Label(self.main_frame, text="Player 1's turn", font=("Helvetica", 12))
+        self.message_label.grid(row=self.y_max +2, column=1)
+        
+        self.reset_button = Button(self.main_frame, text="Reset", command=self.reset_game)
+        self.reset_button.grid(row=self.y_max+3, column=1)
+
 
         self.buttons = []
         for y_grid in range(self.y_max):
             row_of_buttons = []
             for x_grid in range(self.x_max):
-                button = tk.Button(
-                    self.root, text="", width=10, height=5,
+                button = Button(
+                    self.main_frame, text="",font=("Helvetica", 20), width=10, height=5,
                     command=lambda x=x_grid, y=y_grid: self.game_loop_onclick(x, y)
                 )
                 button.grid(row=y_grid, column=x_grid)
@@ -59,7 +83,7 @@ class tic_tac_toe(Grid_Game):
 
         # Start logger
         self.logger = logging.getLogger(__name__)
-        logging.basicConfig(filename='ttt_game.log', encoding='utf-8', level=logging.INFO)
+        logging.basicConfig(filename= str(self.log_directory), encoding='utf-8', level=logging.INFO)
         self.logger.info(f"{datetime.datetime.now()} - New game started")
 
         # Start GUI mainloop.
@@ -71,15 +95,17 @@ class tic_tac_toe(Grid_Game):
         updated_board = self.update_board_state(self.current_board, move)
         updated_board = self.check_win_condition(updated_board)
         self.game_record(updated_board)
+        self.choose_next_player(updated_board)
 
         if updated_board.is_winner:
-            self.message_label.config(text=f"Player {updated_board.current_player} wins!")
+            self.message_label.config(text=f"Player {self.current_board.current_player} wins!")
         elif updated_board.is_tie:
             self.message_label.config(text="It's a tie!")
         else:
             self.message_label.config(text=f"Player {updated_board.current_player}'s turn")
 
-        self.current_board = self.choose_next_player(updated_board)
+        self.current_board = updated_board
+        
 
     def reset_game(self):
         """Resets the game state and UI to start a new game."""
@@ -96,42 +122,15 @@ class tic_tac_toe(Grid_Game):
     def game_record(self, board_update: TTT_Board) -> bool:
         return True
 
-<<<<<<< HEAD
-    def choose_next_player(self, current_board: board_state) -> board_state:
-        """Determines the next player and returns a new board state."""
-        
-        if current_board.current_player < current_board.player_count:
-            return board_state(
-                turn=current_board.turn + 1,
-                current_player=current_board.current_player + 1,
-                moves_played=current_board.board_vectors.copy()
-            )
-        else:
-            return board_state(
-                turn=current_board.turn + 1,
-                current_player=1,
-                moves_played=current_board.board_vectors.copy()
-            )
-
-    def player_move(self, x_dim, y_dim) -> Move:
-        """Creates a Move object from the player's input and updates the button text."""
-=======
     def player_move(self, x_dim, y_dim, piece: tuple = None) -> Move:
->>>>>>> eb544497072bb5c311d18fbe06b7b24a9be878ba
 
         self.buttons[y_dim][x_dim]["state"] = "disabled"
         self.buttons[y_dim][x_dim]["text"] = "X" if self.current_board.current_player == 1 else "O"
         
         return Move(self.current_board.current_player, [x_dim, y_dim])
 
-<<<<<<< HEAD
-    def update_board_state(self, old_board: board_state, played_move: Move) -> board_state:
-        """Returns a new board state object that includes the latest move."""
-
-=======
     def update_board_state(self, old_board: TTT_Board, played_move: Move) -> TTT_Board:
         
->>>>>>> eb544497072bb5c311d18fbe06b7b24a9be878ba
         new_board_vectors: dict = old_board.board_vectors.copy()
         new_board_vectors[f"Turn {old_board.turn}"] = played_move
         return TTT_Board(
@@ -145,7 +144,7 @@ class tic_tac_toe(Grid_Game):
 
         directions = [
             (-1, -1), (-1, 0), (-1, 1),
-            (0, -1),          (0, 1),
+            (0, -1),           (0, 1),
             (1, -1),  (1, 0),  (1, 1)
         ]
 
@@ -171,13 +170,12 @@ class tic_tac_toe(Grid_Game):
 
         # Check for tie after all win conditions are verified, if all spaces are filled and there's no winner, that's a tie.
         if not current_board.is_winner and len(current_board.position_map) == self.x_max * self.y_max:
-            for y_dim in range(3):
-                for x_dim in range(3):
+            for y_dim in range(self.y_max):
+                for x_dim in range(self.x_max):
                     self.buttons[y_dim][x_dim]["state"] = "disabled"
-            game_end_board = current_board
-            game_end_board.is_tie = True
+            current_board.is_tie = True
             print("It's a tie!")
-            return game_end_board
+            return current_board
 
         # Check for winner after all win conditions and  are verified
         if current_board.is_winner:
